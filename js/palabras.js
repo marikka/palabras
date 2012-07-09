3768060e9328bba60c2db0c4fb6c23760b2a0b83
Marek Miettinen <marek.miettinen@gmail.com>
2012-07-09 11:37:58 +0300

    Added backbone localstorage, bootstrap, date.js

--- js/palabras.js
+++ js/palabras.js
@@ -1,5 +1,7 @@
 (function($){
 
+  'use strict';
+
   //////////////////////////////////////////////////////////
   var Word = Backbone.Model.extend({
     defaults: {
@@ -7,6 +9,12 @@
       es: 'palabra',
       rating: 0,
       lastAsked: 'foo'
+    },
+    initialize: function(){
+      this.on('change:rating', this.enforceRatingBounds, this);
+    },
+    enforceRatingBounds: function() {
+      this.set('rating', Math.max(Math.min(this.get('rating'), 100),0));
     }
   });
 
@@ -16,34 +24,44 @@
 
   //////////////////////////////////////////////////////////
   var WordList = Backbone.Collection.extend({
-    model: Word
+    model: Word,
+    localStorage: new Backbone.LocalStorage("SomeCollection"),
+    initialize: function() {
+      this.fetch();
+    } 
   });
 
 
 
 
 
-  //One word row
+  //One word row ///////////////////////////////////////////
   var WordView = Backbone.View.extend({
     tagName: 'tr',
 
     events: {
-      'click button.delete': 'remove'
+      'click a.delete': 'deleteWord'
     },
 
     initialize: function(){
       _.bindAll(this, 'render');
-      this.wordTemplate = _.template('<td><%= fi %></td><td><%= es %></td><td><%= rating %></td><td><%= lastAsked %></td><td><button class="delete" type="button">Delete</button></td>');
+      this.wordTemplate = _.template('<td><%= fi %></td><td><%= es %></td><td><div class="progress"><div class="bar" style="width: <%= rating %>%;"><%= rating %></div></div></td><td><%= lastAsked %></td><td><a class="btn btn-danger delete" type="button"><i class="icon-trash icon-white"></i> Delete</button></td>');
+      this.model.on('change', this.render);
     },
 
     render: function(){
-      $(this.el).html(this.wordTemplate({fi: this.model.attributes.fi, es: this.model.attributes.es, lastAsked: new Date(this.model.attributes.lastAsked).toString(), rating: this.model.attributes.rating}));
+      $(this.el).html(this.wordTemplate({fi: this.model.attributes.fi, es: this.model.attributes.es, lastAsked: new Date(this.model.attributes.lastAsked).toString("dd.mm.yyyy HH:mm") , rating: this.model.attributes.rating}));
       return this;
+    },
+
+    deleteWord: function(){
+      this.model.destroy();
+      this.remove();
     }
   });
 
 
-  //Manage view
+  //Manage view ///////////////////////////////////////////
   var ManageView = Backbone.View.extend({
     el: $('#manage'),
 
@@ -70,11 +88,11 @@
       word.set({
         fi: $("#word-fi", this.el).val(),
         es: $("#word-es", this.el).val(),
-        rating: 0,
         lastAsked: new Date()
       });
       $('form')[0].reset();
       this.collection.add(word);
+      word.save();
       return false;
     },
 
@@ -83,10 +101,54 @@
         model: word
       });
       $('tbody', this.el).append(wordView.render().el);
-    },
+    }
   });
 
+
+  ///////////////////////////////////////////
   var TrainView = Backbone.View.extend({
+    el: $('#train'),
+
+    events: {
+      'submit #answer': 'checkAnswer'
+    },
+
+    initialize: function(){
+      _.bindAll(this, 'render', 'checkAnswer');
+      this.newQuestion();
+      this.render();
+    },
+
+    render: function(){
+      $('#question', this.el).html(this.question.get('fi'));
+    },
+
+    checkAnswer: function(e){
+      e.preventDefault();
+
+
+      if(this.question.get('es') === $("#answerWord", this.el).val())
+      {
+        this.question.save({lastAsked: new Date(), rating: this.question.get('rating') + 1});
+      } else {
+        this.question.save({lastAsked: new Date(), rating: this.question.get('rating') - 1});
+      }
+
+      $('form', this.el)[0].reset();
+
+      
+      this.$el.append('<p><span>'+this.question.get('fi')+'</span>'+' <span>'+this.question.get('es')+'</span></p>');
+
+      this.newQuestion(); //Pick a new word
+
+    },
+
+    newQuestion: function(){
+      this.question = this.collection.shuffle()[0]; //Pick a random word
+      this.render();
+    }
+
+
 
   });
 
